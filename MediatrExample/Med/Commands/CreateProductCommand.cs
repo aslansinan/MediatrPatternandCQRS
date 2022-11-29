39ca057621d0a69343070474b9a0ea34c84689aa
sinan <aslansinan586@gmail.com>
2022-11-29 12:54:15 +0300

    first commit

---
+++ MediatrExample/Med/Commands/CreateProductCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace MediatrExample.Med.Commands
+{
+    public class CreateProductCommand : IRequest<Guid>
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Value { get; set; }
+
+        public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Guid>
+        {
+            public Task<Guid> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(Guid.NewGuid()); 
+                //Repostiroy aracılığı ile veri veritabanına gönderilir
+            }
+        }
+    }
+}
---
+++ MediatrExample/Med/Queries/GetProductByIdHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace MediatrExample.Med.Queries
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, GetProductViewModel>
+    {
+        public Task<GetProductViewModel> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var viewModel = new GetProductViewModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Book"
+            };
+            return Task.FromResult(viewModel);
+            //Get Product from repository
+        }
+    }
+}
